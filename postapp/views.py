8da28ae493915e479d07903d3be44cbb3c7bc398
Joselyne97 <joselynejojo740@gmail.com>
2019-10-25 11:04:52 +0200

    adding database of the user

--- postapp/views.py
+++ postapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from .models import 
+from .forms import 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    
+
+
