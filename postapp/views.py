560dad8d441ac031203cfe0fa2a1d35966455c32
Joselyne97 <joselynejojo740@gmail.com>
2019-10-25 19:59:14 +0200

    I added serializers

--- postapp/views.py
+++ postapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Profile
-from .forms import NewProfileForm
+from .models import Profile,Project
+from .forms import NewProfileForm,NewProjectForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer
+from rest_framework import status
+
 
 
 @login_required(login_url='/accounts/login/')
@@ -13,17 +18,18 @@ def welcome(request):
     current_user = request.user
     user_profile= Profile.objects.filter(user=current_user.id).first()
 
-    return render(request, 'users/index.html', {'user_profile':user_profile, })
+    projects = Project.objects.all()
+    return render(request, 'users/index.html', {'user_profile':user_profile, 'projects':projects})
 
 
 @login_required(login_url='/accounts/login/')
 def user_profile(request):
     current_user = request.user
-    # images = Image.objects.filter(user=current_user).all()
+    projects = Project.objects.filter(user=current_user).all()
     user_profile = Profile.objects.filter(user=current_user.id).first()
     
 
-    return render(request, 'users/user_profile.html', { 'user_profile':user_profile,})
+    return render(request, 'users/user_profile.html', { 'user_profile':user_profile,'projects':projects})
 
 
 
@@ -47,6 +53,32 @@ def edit_profile(request):
 
     return render(request, 'users/edit_profile.html', {'form':form,})
 
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+  
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+
+        return redirect ("welcome")
+
+    else:
+        form = NewProjectForm()
+
+    return render(request, 'users/new_project.html', {"form": form})
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
+
+    
 
 
 
