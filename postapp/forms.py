f815c167ef75e8bd18eab4bd74187f19f78d918a
Joselyne97 <joselynejojo740@gmail.com>
2019-10-25 16:04:53 +0200

    I added the form for editing the profile

--- postapp/forms.py
+++ postapp/forms.py
@@ -1,3 +1,9 @@
 from django import forms
-from .models import 
+from .models import Profile
 from django.contrib.auth.models import User
+
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        exclude =['user']
